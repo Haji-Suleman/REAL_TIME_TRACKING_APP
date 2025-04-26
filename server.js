c2ee05c922b7d074f5d50ab8c278db73db385bad
user <you@example.com>
2025-04-26 09:47:39 +0500

    completed

--- server.js
+++ server.js
@@ -2,24 +2,32 @@ const express = require("express");
 const socketIo = require("socket.io");
 const http = require("http");
 const path = require("path");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", function (socket) {
+  console.log("connected:", socket.id);
+
   socket.on("send-location", function (data) {
     io.emit("recieve-location", { id: socket.id, ...data });
   });
-  console.log("connected");
-  socket.on("disconnect",function(){
-    io.emit("user-disconnected",socket.id)
-  })
+
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+  });
 });
+
 app.get("/", function (req, res) {
   res.render("index");
 });
 
-server.listen(3000);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
